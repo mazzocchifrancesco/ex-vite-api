f0345c05744eb332defb1fcc1be6c5327a97a460
FrancescoMazzocchi <123@email.it>
2023-11-30 17:28:57 +0100

    creazione scheda

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,32 +1,13 @@
 <script>
 import AppCard from './AppCard.vue'
-import { database } from "./database.js" //state management
-import axios from 'axios'; //importo Axios
-
-
-
 
 export default {
     name: "AppMain",
     components: {
-        AppCard,
-    },
-    data() {
-        return {
-            database
-        }
-    },
-    mounted() {
-        this.getBeer()
-    },
-    methods: {
+        AppCard
 
-        getBeer() {
-            axios.get(this.database.apiUrl).then(risultato => {
-                console.log(risultato);
-            });
-        }
-    }
+    },
+
 }
 </script>
 <template>
